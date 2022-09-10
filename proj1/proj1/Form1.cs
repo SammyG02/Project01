37659128de424a8b423038593d647e7acac90b18
SammyG02 <samuelgosaye02@gmail.com>
2022-09-10 21:42:41 +0300

    fixed size

--- proj1/proj1/Form1.cs
+++ proj1/proj1/Form1.cs
@@ -16,5 +16,40 @@ namespace proj1
         {
             InitializeComponent();
         }
+
+        private void btnContact_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlalytics_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
     }
 }
