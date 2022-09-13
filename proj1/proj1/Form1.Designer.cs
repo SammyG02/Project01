b5efb825a874abbef6c1125c1be2d277886b8f7f
SammyG02 <samuelgosaye02@gmail.com>
2022-09-13 10:32:42 +0300

    check

--- proj1/proj1/Form1.Designer.cs
+++ proj1/proj1/Form1.Designer.cs
@@ -55,9 +55,8 @@
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(185, 705);
+            this.panel1.Size = new System.Drawing.Size(278, 1085);
             this.panel1.TabIndex = 0;
             // 
             // btnSettings
@@ -66,10 +65,9 @@
             this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSettings.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSettings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnSettings.Location = new System.Drawing.Point(0, 651);
-            this.btnSettings.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSettings.Location = new System.Drawing.Point(0, 1002);
             this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(172, 52);
+            this.btnSettings.Size = new System.Drawing.Size(258, 80);
             this.btnSettings.TabIndex = 1;
             this.btnSettings.Text = "Logout";
             this.btnSettings.UseVisualStyleBackColor = true;
@@ -81,10 +79,9 @@
             this.btnContact.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnContact.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnContact.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnContact.Location = new System.Drawing.Point(0, 338);
-            this.btnContact.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnContact.Location = new System.Drawing.Point(0, 520);
             this.btnContact.Name = "btnContact";
-            this.btnContact.Size = new System.Drawing.Size(185, 57);
+            this.btnContact.Size = new System.Drawing.Size(278, 88);
             this.btnContact.TabIndex = 1;
             this.btnContact.Text = "Contact Us";
             this.btnContact.UseVisualStyleBackColor = true;
@@ -97,10 +94,9 @@
             this.btnCalander.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCalander.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCalander.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnCalander.Location = new System.Drawing.Point(0, 288);
-            this.btnCalander.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalander.Location = new System.Drawing.Point(0, 443);
             this.btnCalander.Name = "btnCalander";
-            this.btnCalander.Size = new System.Drawing.Size(185, 50);
+            this.btnCalander.Size = new System.Drawing.Size(278, 77);
             this.btnCalander.TabIndex = 1;
             this.btnCalander.Text = "Category";
             this.btnCalander.UseVisualStyleBackColor = true;
@@ -112,10 +108,9 @@
             this.btnAlalytics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAlalytics.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAlalytics.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnAlalytics.Location = new System.Drawing.Point(0, 240);
-            this.btnAlalytics.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAlalytics.Location = new System.Drawing.Point(0, 369);
             this.btnAlalytics.Name = "btnAlalytics";
-            this.btnAlalytics.Size = new System.Drawing.Size(185, 48);
+            this.btnAlalytics.Size = new System.Drawing.Size(278, 74);
             this.btnAlalytics.TabIndex = 1;
             this.btnAlalytics.Text = "Items";
             this.btnAlalytics.UseVisualStyleBackColor = true;
@@ -128,10 +123,9 @@
             this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDashboard.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDashboard.ForeColor = System.Drawing.Color.White;
-            this.btnDashboard.Location = new System.Drawing.Point(0, 189);
-            this.btnDashboard.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDashboard.Location = new System.Drawing.Point(0, 291);
             this.btnDashboard.Name = "btnDashboard";
-            this.btnDashboard.Size = new System.Drawing.Size(185, 51);
+            this.btnDashboard.Size = new System.Drawing.Size(278, 78);
             this.btnDashboard.TabIndex = 1;
             this.btnDashboard.Text = "Home";
             this.btnDashboard.UseVisualStyleBackColor = true;
@@ -143,9 +137,8 @@
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(185, 189);
+            this.panel2.Size = new System.Drawing.Size(278, 291);
             this.panel2.TabIndex = 0;
             // 
             // label2
@@ -153,8 +146,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.SpringGreen;
-            this.label2.Location = new System.Drawing.Point(49, 148);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(92, 244);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 26);
             this.label2.TabIndex = 2;
@@ -166,8 +158,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label1.Location = new System.Drawing.Point(24, 108);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(61, 196);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(144, 25);
             this.label1.TabIndex = 1;
@@ -177,10 +168,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::proj1.Properties.Resources.man;
-            this.pictureBox1.Location = new System.Drawing.Point(35, 2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(52, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(111, 94);
+            this.pictureBox1.Size = new System.Drawing.Size(166, 145);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -190,8 +180,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(1046, 2);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(1569, 3);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(28, 26);
             this.label3.TabIndex = 3;
@@ -200,14 +189,13 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(1079, 705);
+            this.ClientSize = new System.Drawing.Size(1618, 1085);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
